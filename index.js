96a1a390f8f923768b2f0158a11b49329dbd8069
Melissa Fields <melissamfields12@gmail.com>
2022-10-04 12:29:02 -0700

    commit tree 38c8fa227f88e6c249d7f457236dc9f42ba6efe9

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, string){
+    const result = drivers.filter(drivers => drivers.toLowerCase() === string.toLowerCase());
+    return result;
+}
+function fuzzyMatch(drivers, string){
+    const result = drivers.filter(drivers => drivers.startsWith(string));
+    return result;    
+}
+function matchName(drivers, string){
+    const result = drivers.filter(drivers => drivers.name === string);
+    return result; 
+}
